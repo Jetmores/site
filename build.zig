fc4c31f192920a122b4f786b8c903c38735265c6
lets2rs <lets2rs@126.com>
2025-06-22 20:33:37 +0800

    fit for zig-0.14.1

--- build.zig
+++ build.zig
@@ -2,12 +2,9 @@ const std = @import("std");
 const zine = @import("zine");
 
 pub fn build(b: *std.Build) !void {
-    zine.website(b, .{
-        .title = "YuHeng's blog!",
-        .host_url = "https://sample.com",
-        .layouts_dir_path = "layouts",
-        .content_dir_path = "content",
-        .assets_dir_path = "assets",
-        // .static_assets = &.{"ShenJianXin.mp4"},
-    });
+    b.getInstallStep().dependOn(&zine.website(b, .{}).step);
+
+    const serve = b.step("serve", "Start the Zine dev server");
+    const run_zine = zine.serve(b, .{});
+    serve.dependOn(&run_zine.step);
 }
